eae2d7cbcd61e30598fd12b2b1d0a48d690f208c
muhammadadam16 <muhamad.adam_ti21@nusaputra.ac.id>
2025-07-13 20:20:43 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,27 +1,28 @@
 <?php
 
-namespace App\Providers;
+    namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+    use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\Facades\URL;
+    use Illuminate\Support\Facades\App;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    class AppServiceProvider extends ServiceProvider
     {
-        //
-    }
+        /**
+         * Register any application services.
+         */
+        public function register(): void
+        {
+            //
+        }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        if(env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
+        /**
+         * Bootstrap any application services.
+         */
+        public function boot(): void
+        {
+            if(App::environment('production')) {
+                URL::forceScheme('https');
+            }
         }
     }
-}
